5d3df40dd0d83cd219a6e8d4dc543a34f86f920c
Rishi Mukherjee <thethunder666@gmail.com>
2012-06-30 13:16:14 +0530

    Update master

--- canny-slow.py
+++ canny-slow.py
@@ -3,6 +3,7 @@ import scipy
 import numpy
 import Image
 import math
+from math import pi
 
 sigma = 1.4
 f = 'Bikesgray.jpg'
@@ -11,8 +12,8 @@ imgdata = numpy.array(img, dtype = float)
 G = ndi.filters.gaussian_filter(imgdata, sigma)                           #gaussian low pass filter
 
 sobelout = Image.new('L', img.size)                                       #empty image
-sobeloutmag = numpy.array(sobelout, dtype = float)                        
-sobeloutdir = numpy.array(sobelout, dtype = float)
+gradx = numpy.array(sobelout, dtype = float)                        
+grady = numpy.array(sobelout, dtype = float)
 
 sobel_x = [[-1,0,1],
            [-2,0,2],
@@ -39,12 +40,58 @@ for x in range(1, width-1):
              (sobel_y[1][1] * G[x][y]) + (sobel_y[1][2] * G[x+1][y]) + \
              (sobel_y[2][0] * G[x-1][y+1]) + (sobel_y[2][1] * G[x][y+1]) + \
              (sobel_y[2][2] * G[x+1][y+1])
-        mag = math.ceil(math.sqrt((px*px)+(py*py)))
-        direc = math.atan2(py, px)
-        sobeloutmag[x][y] = mag
-        sobeloutdir[x][y] = direc
+        gradx[x][y] = px
+        grady[x][y] = py
 
-scipy.misc.imshow(sobeloutmag)
+sobeloutmag = scipy.hypot(gradx, grady)
+sobeloutdir = scipy.arctan2(grady, gradx)
 
-    
-        
+scipy.misc.imsave('cannynewmag.jpg', sobeloutmag)
+scipy.misc.imsave('cannynewdir.jpg', sobeloutdir)
+
+for x in range(width):
+    for y in range(height):
+        if (sobeloutdir[x][y]<22.5 and sobeloutdir[x][y]>=0) or \
+           (sobeloutdir[x][y]>=157.5 and sobeloutdir[x][y]<202.5) or \
+           (sobeloutdir[x][y]>=337.5 and sobeloutdir[x][y]<=360):
+            sobeloutdir[x][y]=0
+        elif (sobeloutdir[x][y]>=22.5 and sobeloutdir[x][y]<67.5) or \
+             (sobeloutdir[x][y]>=202.5 and sobeloutdir[x][y]<247.5):
+            sobeloutdir[x][y]=45
+        elif (sobeloutdir[x][y]>=67.5 and sobeloutdir[x][y]<112.5)or \
+             (sobeloutdir[x][y]>=247.5 and sobeloutdir[x][y]<292.5):
+            sobeloutdir[x][y]=90
+        else:
+            sobeloutdir[x][y]=135
+
+
+scipy.misc.imsave('cannynewdirquantize.jpg', sobeloutdir)
+
+mag_sup = sobeloutmag.copy()
+
+for x in range(1, width-1):
+    for y in range(1, height-1):
+        if sobeloutdir[x][y]==0:
+            if (sobeloutmag[x][y]<=sobeloutmag[x][y+1]) or \
+               (sobeloutmag[x][y]<=sobeloutmag[x][y-1]):
+                mag_sup[x][y]=0
+        elif sobeloutdir[x][y]==45:
+            if (sobeloutmag[x][y]<=sobeloutmag[x-1][y+1]) or \
+               (sobeloutmag[x][y]<=sobeloutmag[x+1][y-1]):
+                mag_sup[x][y]=0
+        elif sobeloutdir[x][y]==90:
+            if (sobeloutmag[x][y]<=sobeloutmag[x+1][y]) or \
+               (sobeloutmag[x][y]<=sobeloutmag[x-1][y]):
+                mag_sup[x][y]=0
+        else:
+            if (sobeloutmag[x][y]<=sobeloutmag[x+1][y+1]) or \
+               (sobeloutmag[x][y]<=sobeloutmag[x-1][y-1]):
+                mag_sup[x][y]=0
+
+scipy.misc.imsave('cannynewmagsup.jpg', mag_sup)
+
+med = numpy.mean(mag_sup)
+th = 1.33*med
+tl = 0.66*med
+
+
